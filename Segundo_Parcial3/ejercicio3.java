1b6dec898fb74dfd02f5d33da0f60d1cec221f11
valeMercado <202308359@est.umss.edu>
2024-06-16 19:44:41 +0000

    Cambiando ejemplo del ejercicio 3

--- Segundo_Parcial3/ejercicio3.java
+++ Segundo_Parcial3/ejercicio3.java
@@ -3,40 +3,40 @@ public class ejercicio3
    
     public static void mirarArbol() {
         
-        // Ejemplo 1
-        // Tree<String> rootNode = new Tree<String>("c");
+        // Ejemplo de la practica    
+        Tree<String> rootNode = new Tree<String>("c");
         
-        // Tree<String> childA = rootNode.addChild("a");
-        //Tree<String> childU = rootNode.addChild("u");
-        //Tree<String> childH = rootNode.addChild("h");
+        Tree<String> childA = rootNode.addChild("a");
+        Tree<String> childU = rootNode.addChild("u");
+        Tree<String> childH = rootNode.addChild("h");
         
-        //Tree<String> childAT = childA.addChild("t");
-        //Tree<String> childUT = childU.addChild("t");
-        //Tree<String> childHE = childH.addChild("e");
-        //Tree<String> childHU = childH.addChild("u");
+        Tree<String> childAT = childA.addChild("t");
+        Tree<String> childUT = childU.addChild("t");
+        Tree<String> childHE = childH.addChild("e");
+        Tree<String> childHU = childH.addChild("u");
         
-        //Tree<String> childATS = childAT.addChild("s");
-        //Tree<String> childUTP = childUT.addChild("p");
-        //Tree<String> childUTE = childUT.addChild("e");
-        //Tree<String> childHUI = childHU.addChild("i");
-        //Tree<String> childHUS = childHU.addChild("s");  
+        Tree<String> childATS = childAT.addChild("s");
+        Tree<String> childUTP = childUT.addChild("p");
+        Tree<String> childUTE = childUT.addChild("e");
+        Tree<String> childHUI = childHU.addChild("i");
+        Tree<String> childHUS = childHU.addChild("s");  
         
         
-        // Ejemplo 2
+        // Ejemplo 2 (Descomentar para ver este nuevo arbol)
         
-        Tree<String> rootNode = new Tree<String>("a");
+        //Tree<String> rootNode = new Tree<String>("a");
         
-        Tree<String> childB = rootNode.addChild("b");
-        Tree<String> childC = rootNode.addChild("c");
+        //Tree<String> childB = rootNode.addChild("b");
+        //Tree<String> childC = rootNode.addChild("c");
         
-        Tree<String> childBD = childB.addChild("d");
-        Tree<String> childBE = childB.addChild("e");
+        //Tree<String> childBD = childB.addChild("d");
+        //Tree<String> childBE = childB.addChild("e");
         
-        Tree<String> childCF = childC.addChild("f");
-        Tree<String> childCG = childC.addChild("g");
+        //Tree<String> childCF = childC.addChild("f");
+        //Tree<String> childCG = childC.addChild("g");
         
-        Tree<String> childBDH = childBD.addChild("h");
-        Tree<String> childBDI = childBD.addChild("i");
+        //Tree<String> childBDH = childBD.addChild("h");
+        //Tree<String> childBDI = childBD.addChild("i");
  
     
         rootNode.mostrar();
